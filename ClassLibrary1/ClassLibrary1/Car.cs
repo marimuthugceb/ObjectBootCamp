47c84b7e8fe493bee2b820fd4d8d2da54a3f93ec
Marimuthu <marimutm@thoughtworks.com>
2013-06-27 11:41:29 +0530

    Creating new project

---
+++ ClassLibrary1/ClassLibrary1/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Car
+    {
+        public string Color;
+        public int Speed;
+        public string Brand;
+        public string NumberOfSeats;
+        public string CarType;
+        public string EnginePower;
+        public bool IsAutoGear;
+        public int NumberOfGear;
+        public long Price;
+        public string Mileage;
+        public bool Idle;
+        public int Gear;
+
+        public void Accelerate(int Speed)
+        {
+            this.Speed = Speed;
+        }
+
+        public void StopCar()
+        {
+            this.Idle = true;
+        }
+
+
+        public void StartCar()
+        {
+            this.Idle = false;
+        }
+
+        public void ChangeGear(int Gear)
+        {
+            this.Gear = Gear;
+        }
+
+
+    }
+}
